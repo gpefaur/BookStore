6fb563aaff77d91c3810c8c280c172b1c4e38b2d
germanpefaur2 <germanpefaur2@hotmail.com>
2019-09-28 18:18:20 -0300

    Commit varios cambios

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Book Title")]
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "The Title length should be between 2 and 50.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Publish Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+    }
+}
